3a90dfbeaf91262ae6d34261c7c005379e4efd18
Chirag Jain <jainchg@gmail.com>
2019-11-05 02:38:14 +0530

    Added Java classes for remaining fragments

---
+++ app/src/main/java/com/precog/dabbagul/AddLunchFeedbackFragment.java
@@ -0,0 +1,17 @@
+package com.precog.dabbagul;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class AddLunchFeedbackFragment extends BaseFragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_add_lunch, container, false);
+    }
+}
