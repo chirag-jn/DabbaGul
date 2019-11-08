1dd53bc07f94c12742dc79ddac609eb0bb078311
sejalbhalla <sejal17100@iiitd.ac.in>
2019-11-08 18:33:56 +0530

    update food class

---
+++ app/src/main/java/com/precog/dabbagul/Food.java
@@ -0,0 +1,15 @@
+package com.precog.dabbagul;
+
+import com.google.firebase.Timestamp;
+
+import java.util.Map;
+
+public class Food {
+    public String id;
+    public String description;
+    public Timestamp lunch_datetime;
+    public String name;
+    public String owner_email;
+    public String picture;
+    public Map<String, String> tags;
+}
