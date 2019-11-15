d19c7e73cb155d9c7e7825d9a278b0a587244940
Chirag Jain <jainchg@gmail.com>
2019-11-16 01:10:15 +0530

    Modified settings

--- app/src/main/java/com/precog/dabbagul/SettingsActivity.java
+++ app/src/main/java/com/precog/dabbagul/SettingsActivity.java
@@ -1,5 +1,7 @@
 package com.precog.dabbagul;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -11,10 +13,13 @@ public class SettingsActivity extends BaseActivity implements View.OnClickListen
     private static String TAG = "SettingsActivity";
 
     ImageButton returnBack;
-    Button account;
-    Button notifications;
+//    Button account;
+//    Button notifications;
     Button about;
     Button privacy;
+    Button logout;
+
+    Context mContext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,31 +29,47 @@ public class SettingsActivity extends BaseActivity implements View.OnClickListen
         getSupportActionBar().hide();
 
         returnBack = findViewById(R.id.return_from_settings_button);
-        account = findViewById(R.id.settings_account);
-        notifications = findViewById(R.id.settings_notifications);
+//        account = findViewById(R.id.settings_account);
+//        notifications = findViewById(R.id.settings_notifications);
         about = findViewById(R.id.settings_about);
         privacy = findViewById(R.id.settings_privacy);
+        logout = findViewById(R.id.settings_logout);
 
         returnBack.setOnClickListener(this);
-        account.setOnClickListener(this);
-        notifications.setOnClickListener(this);
+//        account.setOnClickListener(this);
+//        notifications.setOnClickListener(this);
         about.setOnClickListener(this);
         privacy.setOnClickListener(this);
+        logout = findViewById(R.id.settings_logout);
+
+        mContext = this;
     }
 
     @Override
     public void onClick(View view) {
+        String website;
+        Intent intent = new Intent(mContext, webActivity.class);
         switch (view.getId()) {
             case R.id.return_from_settings_button:
                 finish();
                 break;
-            case R.id.settings_account:
-                break;
-            case R.id.settings_notifications:
-                break;
+//            case R.id.settings_account:
+//                break;
+//            case R.id.settings_notifications:
+//                break;
             case R.id.settings_privacy:
+                website = "http://home.iiitd.edu.in/~chirag17041/dabbagul/privacy_policy.html";
+                intent.putExtra("web", website);
+                mContext.startActivity(intent);
                 break;
             case R.id.settings_about:
+                website = "http://home.iiitd.edu.in/~chirag17041/dabbagul/index.html";
+                intent.putExtra("web", website);
+                mContext.startActivity(intent);
+                break;
+            case R.id.settings_logout:
+                mAuth.signOut();
+                finish();
                 break;
             default:
                 logv(TAG, "onClick: Kya click kar rhe ho");
---
+++ app/src/main/java/com/precog/dabbagul/webActivity.java
@@ -0,0 +1,32 @@
+package com.precog.dabbagul;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class webActivity extends AppCompatActivity {
+
+    private WebView webview;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_web);
+
+        getSupportActionBar().hide();
+
+        webview =(WebView)findViewById(R.id.webView);
+
+        Intent intent = new Intent();
+        String webUrl = intent.getStringExtra("web");
+
+        webview.setWebViewClient(new WebViewClient());
+        webview.getSettings().setJavaScriptEnabled(true);
+        webview.getSettings().setDomStorageEnabled(true);
+        webview.setOverScrollMode(WebView.OVER_SCROLL_NEVER);
+        webview.loadUrl("https://www.google.com");
+
+    }
+}
